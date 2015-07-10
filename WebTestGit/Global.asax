ea53d19a50e298cac36f3797496ea519ef7dc61a
ppap75 <ppap75@gmail.com>
2015-07-10 09:45:38 +0300

    t1

---
+++ WebTestGit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebTestGit.MvcApplication" Language="C#" %>
---
+++ WebTestGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebTestGit.Startup))]
+namespace WebTestGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
