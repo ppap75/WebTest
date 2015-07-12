12aa9da4b72ea4720b9d6e0fe16796989d79b23e
ppap75 <ppap75@gmail.com>
2015-07-12 15:59:29 +0300

    c3 change

---
+++ WebTestGit/Controllers/BPController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTestGit.Controllers
+{
+    public class BPController : Controller
+    {
+        // GET: BP
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
